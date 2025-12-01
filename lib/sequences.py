4027cd05c92fe01b4851e3dfe68b89b1c4d41844
ronniegikandi <kiiruronnie42@gmil.com>
2025-12-01 11:03:14 +0300

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
-#!/usr/bin/env python3
+
+def fibonacci(length):
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < length:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence
 
 def print_fibonacci(length):
+    fib_sequence = fibonacci(length)
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([fib_sequence[0]])
+    else:
+        print(fib_sequence)
     pass
